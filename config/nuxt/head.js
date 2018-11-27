58ab8e800266228cccfba21e3f365237d88edea3
bruna <brunadafonseca@gmail.com>
2018-11-27 12:05:22 +0100

    feat: load material icons

--- config/nuxt/head.js
+++ config/nuxt/head.js
@@ -10,5 +10,6 @@ module.exports = {
   ],
   link: [
     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons' },
   ]
 }
