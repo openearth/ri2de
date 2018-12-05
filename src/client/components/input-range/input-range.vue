f4f012bac09a3ab34b964951f06572c662b506e7
bruna <brunadafonseca@gmail.com>
2018-12-05 09:52:21 +0100

    feat: create component

---
+++ src/client/components/input-range/input-range.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="input-range">
+    <span class="md-subheader md-theme-default">
+      {{ label }}
+    </span>
+
+    <div class="input-range__input">
+      <vue-slider
+        v-model="val"
+        :width="options.width"
+        :height="options.height"
+        :tooltip-style="options.tooltipStyles"
+        :min-range="0"
+        :max-range="300"
+        tooltip="always"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: [Number, String],
+      default: 0,
+    },
+    min: {
+      type: Number,
+      default: 0,
+    },
+    max: {
+      type: Number,
+      default: 1,
+    },
+  },
+  data() {
+    return {
+      val: [20, 50],
+      options: {
+        width: '100%',
+        height: 8,
+        tooltipStyles: {
+          "height": "25px"
+        },
+      }
+    }
+  },
+  watch: {
+    val(value) {
+      this.$emit('change', value)
+    }
+  },
+}
+</script>
+
+<style>
+.input-range__input {
+  margin-top: 35px;
+}
+
+.input-range__range {
+  flex-grow: 1;
+  margin-right: var(--spacing-default);
+}
+</style>
