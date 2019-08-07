bdc4eb0380debc9bc11d8378ad7bbda0b309775a
ioamicha <33900198+ioamicha@users.noreply.github.com>
2019-08-07 21:05:51 +0200

    created a risk-class component for the classes of the translate to risk functionality, it updates the classes

---
+++ src/client/components/risk-class/risk-class.vue
@@ -0,0 +1,30 @@
+<template>
+  <div >        
+    <input-range
+      :value="[riskclasses[1], riskclasses[2]]"
+      :min="0"
+      :max="3"
+      :interval="0.1"
+      label="Classes"
+      @updateClasses="(classes) =>$emit('updateRiskClasses', classes)"
+      
+    />
+  </div>
+</template>
+
+<script>
+
+import InputRange from '../input-range'
+export default {
+    components:{
+        InputRange,
+    },
+    props:{
+      riskclasses:{
+        type:Array,
+        required: true
+      }
+    }
+}
+</script>
+  
