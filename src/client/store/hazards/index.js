5cc75bcf26e1e4658cdfc0a2f8db211965979734
ioamicha <33900198+ioamicha@users.noreply.github.com>
2019-08-08 15:04:45 +0200

    Revert "Revert "export translate-to-risk, risk-slider, risk -class components""
    This reverts commit c32af02070e8e84f1fdaed21a26bb104303f0ccd.

--- src/client/store/hazards/index.js
+++ src/client/store/hazards/index.js
@@ -4,6 +4,10 @@ export const state = () => ({
   hazards: [],
   selectedHazardIndex: 0,
   susceptibilityFactors: [],
+  bufferDist:50,
+  segmentLength:500,
+  riskClasses:[0,0.2,0.7,3]
+
 })
 
 export const mutations = {
@@ -18,6 +22,15 @@ export const mutations = {
   },
   setHazards(state, hazards) {
     state.hazards = hazards
+  },
+  updateBufferDist(state, bufferDist) {
+    state.bufferDist = Number(bufferDist)
+  },
+  updateSegmentLength(state, segmentLength){
+    state.segmentLength = Number(segmentLength)
+  },
+  updateRiskClasses(state, riskclasses){
+    state.riskClasses = riskclasses
   },
   addSusceptibilityFactorForCurrentHazard(state, susceptibilityFactor) {
     const newFactors = [ ...state.susceptibilityFactors ]
@@ -58,6 +71,7 @@ export const mutations = {
     state.susceptibilityFactors = newFactors
 
   },
+  
   reset(state) {
     state.selectedHazardIndex = 0
     state.susceptibilityFactors = state.susceptibilityFactors.map(susceptibilityFactor =>
--- src/client/store/index.js
+++ src/client/store/index.js
@@ -6,6 +6,8 @@ export const state = () => ({
   completed: {
     infrastructure: false,
     hazards: false,
+    
+
   }
 })
 
@@ -67,6 +69,7 @@ export const actions = {
     dispatch('mapbox/selections/reset')
     dispatch('mapbox/moveMapToCenter')
     dispatch('susceptibility-layers/reset')
+    dispatch('mapbox/features/resetRiskFeatures')
     commit('hazards/reset')
   }
 }
