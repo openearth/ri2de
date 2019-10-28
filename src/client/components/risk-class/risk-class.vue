64266d9585e572984a2d9410a9f76ce29db6d1b2
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-10-28 14:46:16 +0100

    remove unused div in risk-class component

--- src/client/components/risk-class/risk-class.vue
+++ src/client/components/risk-class/risk-class.vue
@@ -1,15 +1,13 @@
-<template>
-  <div >        
-    <input-range
-      :value="[riskclasses[1], riskclasses[2]]"
-      :min="0"
-      :max="3"
-      :interval="0.1"
-      label="Classes"
-      @updateClasses="(classes) =>$emit('updateRiskClasses', classes)"
-      
-    />
-  </div>
+<template>  
+  <input-range
+    :value="[riskclasses[1], riskclasses[2]]"
+    :min="0"
+    :max="3"
+    :interval="0.1"
+    label="Classes"
+    @updateClasses="(classes) =>$emit('updateRiskClasses', classes)"
+    
+  />
 </template>
 
 <script>
