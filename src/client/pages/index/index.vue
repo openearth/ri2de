59564d0bdfabaa10543483e02f0b3d12e3a95670
IoannaMi <ioannamicha@hotmail.com>
2021-06-03 10:26:20 +0200

    Add user agreement; add checkbox; grey out the buttons

--- src/client/pages/index/index.vue
+++ src/client/pages/index/index.vue
@@ -10,61 +10,323 @@
       <md-dialog-title>Welcome</md-dialog-title>
 
       <div class="page-index__logos">
-        <img
-          src="/images/OE_logo.png"
-          alt="OE logo"
-        >
-        <img
-          src="/images/logo-m-2x.png"
-          alt="Deltares logo"
-        >
+        <img src="/images/OE_logo.png" alt="OE logo" />
+        <img src="/images/logo-m-2x.png" alt="Deltares logo" />
       </div>
 
       <md-dialog-content>
-        <p>
-          Ri2de is a webGIS toolkit based on global road datasets [OpenStreetMap] in combination with raster data to calculate road susceptibility [slope, distance to water among others]. The tool is designed to be flexible and configurable to be adapted to every country's road infrastructure evaluation needs.
+        <p style="font-weight: bold">
+          De condities voor het gebruik van RI2DE via de volgende I-Agree User
+          Agreement, voor iedere inlog / bezoek inbouwen.
         </p>
-
+        <p style="font-weight: bold">
+          RI2DE beta website User Agreement Deltares
+        </p>
         <p>
-          Background information on RI2DE as well as the techniques and datasets used in RI2DE can be discovered in the 
-          <a
-            href="https://publicwiki.deltares.nl/display/RI2DE"
-            target="_blank" 
-            rel="noopener"
-          >Public Wiki of RI2DE</a>.
+          You can print and file this legal agreement on the user terms and
+          conditions of this RI2DE website (hereinafter “User Agreement”), e.g.
+          prior to accepting these terms and conditions.
+        </p>
+        <p>Please read this User Agreement carefully.</p>
+        <p>
+          This is a legal agreement between you, acting on behalf of a legal
+          entity, and Stichting Deltares (hereinafter “Deltares”). The use of
+          this RI2DE beta website of Deltares and the resulting outcomes of its
+          use (hereinafter jointly “Website”) is subject to the conditions of
+          this User Agreement as set out below.
+        </p>
+        <p style="font-weight: bold">By marking the “I Agree”-checkbox:</p>
+        <p>
+          <strong>1.</strong> You expressly declare being authorized to act on
+          behalf of the legal entity (hereinafter “User”) you represent for the
+          purposes of accepting this User Agreement;
+        </p>
+        <p>
+          <strong>2.</strong> User expressly accepts this User Agreement and
+          accepts to be legally bound by the terms and conditions contained
+          therein.
+        </p>
+        <p>
+          If you are not authorized to act on behalf of User to agree upon this
+          User Agreement, please do not mark the “I agree” checkbox and exit
+          this Website. Furthermore do not mark the “I Accept” checkbox and end
+          the visit to this Website if User does not agree with the User
+          Agreement.
+        </p>
+        <p>
+          You are not allowed to use this Website without an agreement with
+          Deltares. If you do not agree to the terms of this User Agreement, you
+          must promptly stop using the Website. Any further actions with respect
+          to the Website without an agreement with Deltares are in violation of
+          copyright law.
+        </p>
+        <p style="font-weight: bold">Article 1: Purpose of the Website</p>
+        <p>
+          1.1 &nbsp;&nbsp;&nbsp;&nbsp; This beta-Website was initiated as an
+          internal knowledge tool for Deltares employees and is in keeping with
+          Deltares’ policy on the sharing knowledge, made available to the
+          public as a beta-version, hereinafter “the Website”.
+        </p>
+        <p>
+          1.2 &nbsp;&nbsp;&nbsp;&nbsp; This Website aims to provide users help
+          in identifying locations in their road network that are susceptible to
+          natural hazards. RI2DE stands for <strong>R</strong>isk
+          <strong>I</strong>ndicators for <strong>I</strong>nfrastructure in
+          <strong>D</strong>atascarce <strong>E</strong>nvironments and as such
+          is geared towards hazards and locations where little hazard
+          information is available. The website identifies the susceptibility of
+          infrastructure networks by combining several information layers
+          relating to assets and infrastructure, and publicly available data
+          about the environment. The user can fine tune the input to align with
+          the local situation. The subsequent results should be calibrated using
+          historical data and expert judgement prior to using the results. The
+          results are displayed graphically on the Website and can be locally
+          saved.
+        </p>
+        <p>
+          1.3 &nbsp;&nbsp;&nbsp;&nbsp; Intended users are experts from
+          government agencies e.g. national road authorities, road owners and
+          other affiliated infrastructure organisations e.g. infrastructure
+          experts, maintenance organisations, students and anyone interested.
+        </p>
+        <p>
+          1.4 &nbsp;&nbsp;&nbsp;&nbsp; You can save the results of your session
+          in RI2DE as a file (Report) on the device with which you are logged
+          into the Website. Use of the results of a previous session is possible
+          by opening this Report (file) with this Website, on this device or on
+          any other computer. RI2DE is available for independent use.
+        </p>
+        <p style="font-weight: bold">Article 2: License</p>
+        <p>
+          Deltares grants User a non-exclusive, worldwide, royalty-free,
+          non-transferable and non-sublicensable license to use the Website
+          strictly for the purpose and in accordance with the limitations
+          specified in this User Agreement. This license is provided solely
+          under the condition that User complies with the provisions of this
+          User Agreement.
+        </p>
+        <ol class="lower_alpha">
+          <li>
+            This Website is a beta-version of which the working nor the accuracy
+            of the results is not guaranteed;
+          </li>
+          <li>
+            that User is only authorized to use the Website within its own
+            organisation and for its own use;
+          </li>
+          <li>
+            that the use of this Website and the results is at User’s own risk
+            and expense;
+          </li>
+          <li>
+            to interpret the results of this Website with care to ensure not to
+            conclude false understandings of possible impacts;
+          </li>
+          <li>
+            that User remains solely responsible for the interpretation of the
+            results of the Website and the decisions made based on the result of
+            the Website;
+          </li>
+          <li>
+            never to (re-)sell, re-distribute, make available, grant access to
+            or communicate this Website, nor User’s details, nor the results to
+            this Website, to any third party;
+          </li>
+          <li>
+            the Website may only be used for legitimate purposes, in a manner
+            that cannot in any way (i) harm Deltares or (ii) infringe the rights
+            of third parties.
+          </li>
+        </ol>
+        <p style="font-weight: bold">Article 4: Intellectual Property Rights</p>
+        <p>
+          4.1 &nbsp;&nbsp;&nbsp;&nbsp; Unless stated differently, all
+          intellectual property rights in connection with the Website, including
+          the software, texts, images and sounds, are vested exclusively in
+          Deltares in its affiliates or its contractors. The Website refers to
+          several other background information websites, including the websites:
+        </p>
+        <ol style="list-style-type: none">
+          <li>
+            KEA at
+            <a href="https://www.Klimaateffectatlas.nl" target="_blank"
+              >www.Klimaateffectatlas.nl</a
+            >
+          </li>
+          <li>
+            PDOK (Public Services on the map) at
+            <a href="https://geodata.nationalgeoregister.nl" target="_blank"
+              >https://geodata.nationalgeoregister.nl</a
+            >
+          </li>
+          <li>
+            AHN2 (Current Altitude File for the Netherlands) at
+            <a href="https://geodata.nationalgeoregister.nl" target="_blank"
+              >https://geodata.nationalgeoregister.nl</a
+            >
+          </li>
+          <li>
+            Mapbox (Satellite data / maps ) from
+            <a href="https://www.Mapbox.com" target="_blank">www.Mapbox.com</a>,
+            including the map viewer
+          </li>
+        </ol>
+        <p>
+          All conditions related to the use of these websites and information
+          obtained therefrom also apply to the use of the Website.
+        </p>
+        <p>
+          The foregoing implies, among other things, that without the prior
+          written permission of Deltares it is not permitted to publish,
+          reproduce, copy, modify and / or adapt the information on the Website,
+          except for personal, non-commercial use or to the extent permitted
+          under mandatory law. The User acknowledges and agrees that no rights
+          of ownership, including intellectual property rights, will be
+          transferred to the User with this User Agreement.
+        </p>
+        <p>
+          4.2 &nbsp;&nbsp;&nbsp;&nbsp; The results of your RI2DE sessions can
+          only be saved on your own device. Deltares has no access to these
+          results and is unable to recover lost results from your previous
+          session(s).
+        </p>
+        <p style="font-weight: bold">Article 5: No warranty, indemnification</p>
+        <p>
+          5.1 &nbsp;&nbsp;&nbsp;&nbsp; This Website is a beta-version of which
+          the working nor the accuracy of the results is guaranteed, and the
+          resulting forecasts are provided “AS-IS” without any warranty of any
+          kind. Deltares does not guarantee, or imply in any way, that the
+          Website calculations, flood risk insight and forecasts will correspond
+          to the factual situation
+        </p>
+        <p>
+          5.2 &nbsp;&nbsp;&nbsp;&nbsp; Deltares hereby expressly waives any
+          warranty, implied or explicit, regarding the suitability of the
+          Website for a particular purpose, the usability and merchantability of
+          the results, or regarding the failure of the performance of the
+          website and the Results. Deltares disclaims all liability for any loss
+          or damage suffered as a result of User's use of, or reliance on, this
+          Website and / or the results of your use thereof.
+        </p>
+        <p>
+          5.3 &nbsp;&nbsp;&nbsp;&nbsp; To the best of Deltares’ knowledge and
+          belief, the Website does not infringe any third-party copyright or any
+          other intellectual property. Any responsibility or liability of
+          Deltares for claims, costs, expenses and/or consequential losses or
+          damages resulting from or arising out of any infringement of
+          intellectual property rights is excluded.
+        </p>
+        <p>
+          5.4 &nbsp;&nbsp;&nbsp;&nbsp; User shall indemnify, hold harmless and
+          defend Deltares against any action brought by a third party against
+          Deltares and/or its officers, directors, employees or agents to the
+          extent that such a claim is connected to the use of the Website and/or
+          the results by User and/or third parties at whose disposal User has
+          placed the results.
+        </p>
+        <p style="font-weight: bold">Article 6: Duration</p>
+        <p>
+          6.1 &nbsp;&nbsp;&nbsp;&nbsp; This User Agreement is effective for the
+          duration of your Website session, except that Articles 4, 5 and 7 will
+          continue to apply thereafter.
+        </p>
+        <p style="font-weight: bold">Article 7: Liability</p>
+        <p>
+          7.1 &nbsp;&nbsp;&nbsp;&nbsp; Deltares shall not be liable for any
+          damages resulting directly or indirectly from a shortcoming
+          attributable to Deltares in the performance of its work in restoring,
+          optimizing and improving of the functioning of the Website. Deltares
+          shall never be liable for any damages resulting from the application
+          and/or use of the Website, unless such damages are caused by an
+          intentional act or wilful recklessness of Deltares.
+        </p>
+        <p>
+          7.2 &nbsp;&nbsp;&nbsp;&nbsp; The User acknowledges that Deltares
+          (including its personnel and non-employees who undertake - or have
+          undertaken - activities for or on behalf of Deltares) is not liable to
+          the User for loss of profit, direct, indirect, incidental and / or
+          consequential loss, including theory of liability therefore arising
+          out of the use of the Website, except for damages caused by
+          intentional act or wilful recklessness of Deltares.
+        </p>
+        <p style="font-weight: bold">Article 8: Privacy</p>
+        <p>
+          Any and all personal data that User supplies to Deltares will be
+          processed in accordance with the General Data Protection Regulation.
+          Our Privacy Statement sets out why we collect the data and how it will
+          be processed and protected. Please read the statement carefully,
+          available on our Website here.
+        </p>
+        <p style="font-weight: bold">Article 9: Miscellaneous</p>
+        <p>
+          9.1 &nbsp;&nbsp;&nbsp;&nbsp; The parties are not allowed to assign any
+          rights and/or obligations under the User Agreement, entirely or in
+          part, to third parties without the prior written consent of the other
+          party.
+        </p>
+        <p>
+          9.2 &nbsp;&nbsp;&nbsp;&nbsp; Any disputes arising from and/or
+          connected to the User Agreement, and any agreements arising therefrom,
+          shall be submitted solely to the competent court of The Hague.
+        </p>
+        <p>
+          9.3 &nbsp;&nbsp;&nbsp;&nbsp; User agrees that the Website and/or the
+          resulting forecasts do not give rise to personal jurisdiction over
+          Deltares, either specific or personal, in jurisdictions other than the
+          laws of the Netherlands. This Agreement and all the agreements arising
+          therefrom are governed exclusively by the laws of the Netherlands.
+        </p>
+        <p style="font-weight: bold">About Deltares</p>
+        <p>
+          Deltares has its registered office in Delft and its principal place of
+          business at Boussinesqweg 1, 2629 HV Delft, The Netherlands. Deltares
+          is registered with the Chamber of Commerce and Industry under number
+          41146461. Other contact details are: telephone: + 31 (0)88 335 8273 ;
+          fax: + 31 (0)88 335 8582 ; email:info@deltares.nl.
         </p>
       </md-dialog-content>
-
+      <div />
+      "
+      <div class="page-index__checkbox">
+        <md-checkbox v-model="accepted" class="md-primary"
+          >I agree with the conditions of Use</md-checkbox
+        >
+      </div>
       <div class="page-index__actions">
         <md-button
+          :disabled="!accepted"
           to="/project"
           class="md-raised md-primary"
         >
           New project
         </md-button>
         <div class="page-index__divider" />
-        <import-button />
+        <import-button :accepted="accepted" />
       </div>
-
     </md-dialog>
   </div>
 </template>
 
 <script>
-import ImportButton from '../../components/import-button'
-import { mapState } from 'vuex';
+import ImportButton from "../../components/import-button";
+import { mapState } from "vuex";
 
 export default {
   components: {
-    ImportButton
+    ImportButton,
+  },
+  data() {
+    return {
+      accepted: false,
+    };
   },
   computed: {
-    ...mapState('mapbox/selections', [ 'selections' ]),
+    ...mapState("mapbox/selections", ["selections"]),
   },
   mounted() {
-    this.$store.commit('setActivePage', 'index')
-  }
-}
+    this.$store.commit("setActivePage", "index");
+  },
+};
 </script>
 
 <style>
@@ -81,7 +343,6 @@ export default {
   background-color: lightgrey;
   flex: 1;
 }
-
 .page-index__logos {
   display: flex;
   padding: 0 1.5rem 1.5rem;
@@ -94,5 +355,21 @@ export default {
 
 .page-index__dialog {
   max-width: 40rem;
+}
+.page-index__checkbox {
+  padding: 0rem 1.5rem 1.5rem;
+  border-top-style: solid;
+  border-width: thin;
+  border-color: lightgrey;
+}
+ol.lower_alpha {
+  counter-reset: list;
+}
+ol.lower_alpha > li {
+  list-style: none;
+}
+ol.lower_alpha > li:before {
+  content: counter(list, lower-alpha) ") ";
+  counter-increment: list;
 }
 </style>
